ea33e7047e529e40114eaa22cbdd55579e7150e6
Georgvit <ifreyd@mail.ru>
2023-12-04 22:50:49 +0300

    Initial commit

---
+++ src/main/java/TestProcessor.java
@@ -0,0 +1,77 @@
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestProcessor {
+
+    /**
+     * Данный метод находит все void методы без аргументов в классе, и запускеет их.
+     * <p>
+     * Для запуска создается тестовый объект с помощью конструткора без аргументов.
+     */
+    public static void runTest(Class<?> testClass) {
+        final Constructor<?> declaredConstructor;
+        try {
+            declaredConstructor = testClass.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException("Для класса \"" + testClass.getName() + "\" не найден конструктор без аргументов");
+        }
+
+        final Object testObj;
+        try {
+            testObj = declaredConstructor.newInstance();
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Не удалось создать объект класса \"" + testClass.getName() + "\"");
+        }
+
+        List<Method> methods = new ArrayList<>();
+        List<Method> methodsBeforeEach = new ArrayList<>();
+        List<Method> methodsAfterEach = new ArrayList<>();
+        for (Method method : testClass.getDeclaredMethods()) {
+            if(method.isAnnotationPresent(Skip.class)){
+            }else
+            if (method.isAnnotationPresent(Test.class)) {
+                checkTestMethod(method);
+                methods.add(method);
+            } else
+            if (method.isAnnotationPresent(BeforeEach.class)) {
+                checkTestMethod(method);
+                methodsBeforeEach.add(method);
+            } else
+            if (method.isAnnotationPresent(AfterEach.class)) {
+                checkTestMethod(method);
+                methodsAfterEach.add(method);
+            }
+        }
+
+        methodsBeforeEach.forEach(it -> runTest(it, testObj));
+
+        methods.stream()
+                .sorted(Comparator.comparingInt(x -> x.getAnnotation(Test.class).order()))
+                .forEach(it -> runTest(it, testObj));
+
+        methodsAfterEach.forEach(it -> runTest(it, testObj));
+    }
+
+
+
+    private static void checkTestMethod(Method method) {
+        if (!method.getReturnType().isAssignableFrom(void.class) || method.getParameterCount() != 0) {
+            throw new IllegalArgumentException("Метод \"" + method.getName() + "\" должен быть void и не иметь аргументов");
+        }
+    }
+
+    private static void runTest(Method testMethod, Object testObj) {
+        try {
+            testMethod.invoke(testObj);
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException("Не удалось запустить тестовый метод \"" + testMethod.getName() + "\"");
+        }
+    }
+
+}
+
